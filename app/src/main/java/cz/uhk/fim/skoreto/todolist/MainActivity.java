c6d51149748d3fb1aa6eb362e163c993e01cc12a
Skoreto <TomSkorepa@gmail.com>
2016-03-20 20:52:04 +0100

    First commit. Initializing project.

---
+++ app/src/main/java/cz/uhk/fim/skoreto/todolist/MainActivity.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.skoreto.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
