ee1807db9dd6c2b556b3462c266e11d80f600817
Skoreto <TomSkorepa@gmail.com>
2016-04-08 20:40:22 +0200

    Added implementation of custom TaskAdapter.

--- app/src/main/java/cz/uhk/fim/skoreto/todolist/MainActivity.java
+++ app/src/main/java/cz/uhk/fim/skoreto/todolist/MainActivity.java
@@ -1,43 +1,60 @@
 package cz.uhk.fim.skoreto.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-//import android.support.v7.app.widget.Toolbar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-
-    ArrayAdapter<String> arrayAdapter;
-
-    Database database;
+    ArrayAdapter<Task> arrayAdapter;
+    DataModel dataModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database = new Database(this);
-        listView = (ListView) findViewById(R.id.listView);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, database.getAllTasks());
-
+        dataModel = new DataModel(this);
+        listView = (ListView) findViewById(R.id.lvUsers);
+        arrayAdapter = new TaskAdapter(MainActivity.this, dataModel.getAllTasks());
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Po klepnuti na polozku seznamu.
+                Task task = (Task) listView.getItemAtPosition(position);
+                String taskName = task.getName();
+                Toast.makeText(MainActivity.this, "You selected : " + taskName , Toast.LENGTH_SHORT).show();
+
+                Intent editTaskIntent = new Intent(getApplication(), EditTask.class);
+                editTaskIntent.putExtra("taskId", task.getId());
+                startActivity(editTaskIntent);
+            }
+        });
 
         Button btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                database.saveTask("Nejaky ukol", "Ukolem tohoto ukolu je provest nejakou cinnost.", 1, 0);
+                dataModel.saveTask("Nejaky ukol", "Ukolem tohoto ukolu je provest nejakou cinnost.", 1, 0);
 
                 arrayAdapter.clear();
-                arrayAdapter.addAll(database.getAllTasks());
+                arrayAdapter.addAll(dataModel.getAllTasks());
             }
         });
 
+
     }
+
+
+
 }
---
+++ app/src/main/java/cz/uhk/fim/skoreto/todolist/TaskAdapter.java
@@ -0,0 +1,39 @@
+package cz.uhk.fim.skoreto.todolist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Tomas.
+ */
+public class TaskAdapter extends ArrayAdapter<Task> {
+
+    public TaskAdapter(Context context, ArrayList<Task> tasks) {
+        super(context, 0, tasks);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Task task = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.task_item, parent, false);
+        }
+
+        TextView tvTaskId = (TextView) convertView.findViewById(R.id.tvTaskId);
+        TextView tvTaskName = (TextView) convertView.findViewById(R.id.tvTaskName);
+
+        tvTaskId.setText(task.getDescription());
+        tvTaskName.setText(task.getName());
+
+        return convertView;
+    }
+
+}
