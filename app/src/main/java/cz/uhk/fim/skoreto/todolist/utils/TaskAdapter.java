a924b5d489000a437f00700e7680562c696ed473
Skoreto <TomSkorepa@gmail.com>
2016-04-09 16:55:23 +0200

    Add implementation for checking completed tasks
    Fixed too long names of tasks in ListView.

--- app/src/main/java/cz/uhk/fim/skoreto/todolist/utils/TaskAdapter.java
+++ app/src/main/java/cz/uhk/fim/skoreto/todolist/utils/TaskAdapter.java
@@ -5,11 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import cz.uhk.fim.skoreto.todolist.R;
+import cz.uhk.fim.skoreto.todolist.model.DataModel;
 import cz.uhk.fim.skoreto.todolist.model.Task;
 
 /**
@@ -17,24 +20,74 @@ import cz.uhk.fim.skoreto.todolist.model.Task;
  */
 public class TaskAdapter extends ArrayAdapter<Task> {
 
+    DataModel dm = new DataModel(getContext());
+
+    private class ViewHolder {
+        TextView tvTaskName;
+        CheckBox chbTaskCompleted;
+    }
+
     public TaskAdapter(Context context, ArrayList<Task> tasks) {
         super(context, 0, tasks);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
-        Task task = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder holder = null;
+
+        // Ziskej data pro ukol z teto pozice.
+        final Task task = getItem(position);
+
+        // Over, zda se znovupouziva existujici view, jinak inflatuj toto view.
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.task_item, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.tasks_list_item, parent, false);
+
+            holder = new ViewHolder();
+            holder.tvTaskName = (TextView) convertView.findViewById(R.id.tvTaskName);
+            holder.chbTaskCompleted = (CheckBox) convertView.findViewById(R.id.chbTaskCompleted);
+
+            // Odskrtni checkboxy ukolu, podle toho, zda jsou splneny.
+            if (task.getCompleted() == 0){
+                holder.chbTaskCompleted.setChecked(false);
+            } else {
+                holder.chbTaskCompleted.setChecked(true);
+            }
+
+            convertView.setTag(holder);
+
+            holder.chbTaskCompleted.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    CheckBox chb = (CheckBox) v;
+
+                    // Po kliknuti na checkbox zjisti jeho stav a dle toho prenastav splneni ukolu.
+                    if (chb.isChecked()) {
+                        task.setCompleted(1);
+                    } else {
+                        task.setCompleted(0);
+                    }
+
+                    // Aktualizuj ukol v databazi.
+                    dm.updateTask(task);
+
+                    // Informuj uzivatele o provedene zmene stavu ukolu.
+                    String taskState = "neurcen";
+                    if (task.getCompleted() == 0)
+                        taskState = "nesplněn";
+                    if (task.getCompleted() == 1)
+                        taskState = "splněn";
+                    Toast.makeText(getContext(), "Úkol " + task.getName() + " je " + taskState, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        TextView tvTaskName = (TextView) convertView.findViewById(R.id.tvTaskName);
-//        TextView tvTaskId = (TextView) convertView.findViewById(R.id.tvTaskId);
-
-        tvTaskName.setText(task.getName());
-//        tvTaskId.setText(task.getDescription());
+        // Zamezeni preteceni nazvu ukolu v uvodnim seznamu.
+        if (task.getName().length() > 40) {
+            holder.tvTaskName.setText(task.getName().substring(0, 40) + " ...");
+        } else {
+            holder.tvTaskName.setText(task.getName());
+        }
 
         return convertView;
     }
