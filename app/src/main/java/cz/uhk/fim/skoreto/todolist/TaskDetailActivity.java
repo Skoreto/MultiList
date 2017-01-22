88d52f1d5564548b0063efb66f796fbc26e762fe
Skoreto <TomSkorepa@gmail.com>
2017-01-22 11:02:28 +0100

    Remove ArrayListFragment implementation

--- app/src/main/java/cz/uhk/fim/skoreto/todolist/TaskDetailActivity.java
+++ app/src/main/java/cz/uhk/fim/skoreto/todolist/TaskDetailActivity.java
@@ -213,7 +213,6 @@ public class TaskDetailActivity extends AppCompatActivity {
         if (showDailyWeather) {
             detailTabLayout.addTab(detailTabLayout.newTab().setText("Daily"));
         }
-//        detailTabLayout.addTab(detailTabLayout.newTab().setText("List"));
         detailTabLayout.setTabGravity(TabLayout.MODE_SCROLLABLE);
 
         // Inicializace adapteru FragmentPageru
@@ -460,8 +459,6 @@ public class TaskDetailActivity extends AppCompatActivity {
 //                    return WeatherHourFragment.newInstance(weatherHour);
                 case 4:
                     return WeatherDailyFragment.newInstance(listWeatherDaily, task);
-//                case 4:
-//                    return ArrayListFragment.newInstance(position);
                 default:
                     return null;
             }
@@ -483,8 +480,6 @@ public class TaskDetailActivity extends AppCompatActivity {
 //                    return "Hour";
                 case 4:
                     return "Daily";
-//                case 4:
-//                    return "List";
                 default:
                     return "Page " + position;
             }
@@ -923,63 +918,6 @@ public class TaskDetailActivity extends AppCompatActivity {
             return view;
         }
     }
-
-    public static class ArrayListFragment extends ListFragment {
-        int mNum;
-        String[] CHEESES = {
-                "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance" };
-
-        /**
-         * Create a new instance of CountingFragment, providing "num"
-         * as an argument.
-         */
-        static ArrayListFragment newInstance(int num) {
-            ArrayListFragment f = new ArrayListFragment();
-
-            // Supply num input as an argument.
-            Bundle args = new Bundle();
-            args.putInt("num", num);
-            f.setArguments(args);
-
-            return f;
-        }
-
-        /**
-         * When creating, retrieve this instance's number from its arguments.
-         */
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            mNum = getArguments() != null ? getArguments().getInt("num") : 1;
-        }
-
-        /**
-         * The Fragment's UI is just a simple text view showing its
-         * instance number.
-         */
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View v = inflater.inflate(R.layout.fragment_pager_list, container, false);
-            View tv = v.findViewById(R.id.text);
-            ((TextView)tv).setText("Fragment #" + mNum);
-            return v;
-        }
-
-        @Override
-        public void onActivityCreated(Bundle savedInstanceState) {
-            super.onActivityCreated(savedInstanceState);
-            setListAdapter(new ArrayAdapter<String>(getActivity(),
-                    android.R.layout.simple_list_item_1, CHEESES));
-        }
-
-        @Override
-        public void onListItemClick(ListView l, View v, int position, long id) {
-            Log.i("FragmentList", "Item clicked: " + id);
-        }
-    }
-
-
 
 
 }
